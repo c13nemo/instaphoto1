5a73294ca5b24f40490ae3c083a921ad503bb94a
c13nemo <c13nemo@yahoo.com>
2018-05-29 12:15:35 +0200

    Pages and Navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
+  root 'pages#landing'
+  get 'pages/about' 
+   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
